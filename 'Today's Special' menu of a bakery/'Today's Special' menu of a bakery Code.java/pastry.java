9764ccec3f7e8a5548b82cb9ec9bed67b7dc3b7b
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-02-21 02:16:53 +0530

    Delete 'Today's Special' menu of a bakery/'Today's Special' menu of a bakery Code.java directory

--- 'Today's Special' menu of a bakery/'Today's Special' menu of a bakery Code.java/pastry.java
+++
@@ -1,9 +0,0 @@
-package com.internshala.javaapp;
-
-public class Pastry extends Cake {
-
-	@Override
-	public void display() {
-		System.out.println("  " + name + " : " + '\u20B9' + " " + price + " per piece");
-	}
-}
